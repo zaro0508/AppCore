ada7f819d19f9f41f7acd6430424dc5418c30140
Shannon Young <shannon.young@sagebase.org>
2016-05-26 17:27:54 -0700

    Use keys in localization

--- APCAppCore/APCAppCore/DataSubstrate/Model/APCUser+Bridge.m
+++ APCAppCore/APCAppCore/DataSubstrate/Model/APCUser+Bridge.m
@@ -642,10 +642,10 @@ static NSString* const kBaseEmailAddressForExternalIdKey = @"baseEmailAddressFor
     
     void (^showVcBlock)() = ^
     {
-        NSString* yesStr = NSLocalizedStringWithDefaultValue(@"YES", @"APCAppCore", APCBundle(), @"YES", @"Positive Answer");
-        NSString* noStr = NSLocalizedStringWithDefaultValue(@"NO", @"APCAppCore", APCBundle(), @"NO", @"Negative Answer");
-        NSString* title = NSLocalizedStringWithDefaultValue(@"Are you a tester?", @"APCAppCore", APCBundle(), @"Are you a tester?", @"Question if the user is a quality assurance tester");
-        NSString* msgFormat = NSLocalizedStringWithDefaultValue(@"Based on your email address, we have detected you are a tester for %@.  If this is correct, select %@ so we can store your data separately.", @"APCAppCore", APCBundle(), @"Based on your email address, we have detected you are a tester for %@.  If this is correct, select %@ so we can store your data separately.", @"Message informing user if and what happens if they are a tester");
+        NSString* yesStr = NSLocalizedStringWithDefaultValue(@"APC_YES", @"APCAppCore", APCBundle(), @"Yes", @"Positive Answer");
+        NSString* noStr = NSLocalizedStringWithDefaultValue(@"APC_NO", @"APCAppCore", APCBundle(), @"No", @"Negative Answer");
+        NSString* title = NSLocalizedStringWithDefaultValue(@"APC_TESTER_PROMPT_TITLE", @"APCAppCore", APCBundle(), @"Are you a tester?", @"Question if the user is a quality assurance tester");
+        NSString* msgFormat = NSLocalizedStringWithDefaultValue(@"APC_TESTER_PROMPT_MESSAGE_FORMAT%@_%@", @"APCAppCore", APCBundle(), @"Based on your email address, we have detected you are a tester for %@.  If this is correct, select %@ so we can store your data separately.", @"Message informing user if and what happens if they are a tester");
         NSString* msg = [NSString stringWithFormat:msgFormat, [APCUtilities appName], [yesStr lowercaseString]];
         
         UIAlertController* alert = [UIAlertController alertControllerWithTitle:title
